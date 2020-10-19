8666ba2242d5862c08bbfde027a383a8df2afa3c
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-10-19 11:26:15 -0500

    Makes a Sortable for each dropzone

---
+++ assets/js/dragHook.js
@@ -0,0 +1,21 @@
+import Sortable from 'sortablejs';
+
+export default {
+  mounted() {
+    let dragged;
+    const hook = this;
+
+    const selector = '#' + this.el.id;
+
+    document.querySelectorAll('.dropzone').forEach((dropzone) => {
+      new Sortable(dropzone, {
+        animation: 0,
+        delay: 50,
+        delayOnTouchOnly: true,
+        group: 'shared',
+        draggable: '.draggable',
+        ghostClass: 'sortable-ghost',
+      });
+    });
+  },
+};
