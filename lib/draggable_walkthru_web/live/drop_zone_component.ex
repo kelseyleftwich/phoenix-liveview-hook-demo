5bf5299f2c08d78677b398c0b3dd80d61f915667
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-10-19 09:46:10 -0500

    Adds assigns and markup

---
+++ lib/draggable_walkthru_web/live/drop_zone_component.ex
@@ -0,0 +1,22 @@
+
+defmodule DraggableWalkthruWeb.PageLive.DropZoneComponent do
+  use Phoenix.LiveComponent
+
+  @impl true
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+      <div class="dropzone grid gap-3 p-6 border-solid border-2 border-<%= @color %>-300 rounded-md my-6" id="<%= @drop_zone_id %>">
+        <%= @title %>
+        <%= for %{text: text, id: id} <- @draggables do %>
+          <div draggable="true" id="<%= id %>" class="draggable p-4 bg-<%= @color %>-700 text-white"><%= text %></div>
+        <% end %>
+      </div>
+    """
+  end
+
+end
--- lib/draggable_walkthru_web/live/page_live.ex
+++ lib/draggable_walkthru_web/live/page_live.ex
@@ -3,37 +3,22 @@ defmodule DraggableWalkthruWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    # this is hardcoded but would come from a datasource
+    draggables = [
+      %{id: "drag-me-0", text: "Drag Me 0"},
+      %{id: "drag-me-1", text: "Drag Me 1"},
+      %{id: "drag-me-2", text: "Drag Me 2"},
+      %{id: "drag-me-3", text: "Drag Me 3"},
+    ]
+
+    socket =
+      socket
+      |> assign(:pool, draggables)
+      |> assign(:drop_zone_a, [])
+      |> assign(:drop_zone_b, [])
+
+    {:ok, socket}
   end
 
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not DraggableWalkthruWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
-  end
 end
